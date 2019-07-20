1f4a09ec2813f6b9c25b7c5802e5859dc7315486
Winston  Chung <18.chung.winston@ctkprep.org>
2019-07-20 19:05:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    bis = "and " + arg[-1]
-    arg[-1]= bis
+    bis = "and " + array[-1]
+    array[-1]= bis
     puts arg.join(", ")
   end
 end
